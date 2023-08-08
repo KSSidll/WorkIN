d8cd1abd6828fc1094e3b53f9545c34004bd4b15
KSSidll <kssidll@users.noreply.github.com>
2023-08-08 18:51:59 +0200

    Update Light theme coloring

--- app/src/main/java/com/kssidll/workin/ui/theme/theme/Light.kt
+++ app/src/main/java/com/kssidll/workin/ui/theme/theme/Light.kt
@@ -2,10 +2,10 @@ package com.kssidll.workin.ui.theme.theme
 
 import androidx.compose.ui.graphics.*
 
-val light_primary = Color(0xFFF1F2FC)
-val light_onPrimary = Color(0xFF1B1B1F)
-val light_primaryContainer = Color(0xFFB8AFC8)
-val light_onPrimaryContainer = Color(0xFF2D0E34)
+val light_primary = Color(0xFF495D92)
+val light_onPrimary = Color(0xFFFFFFFF)
+val light_primaryContainer = Color(0xFFDAE2FF)
+val light_onPrimaryContainer = Color(0xFF001849)
 val light_secondary = Color(0xFF625B71)
 val light_onSecondary = Color(0xFFFFFFFF)
 val light_error = Color(0xFFB3261E)
