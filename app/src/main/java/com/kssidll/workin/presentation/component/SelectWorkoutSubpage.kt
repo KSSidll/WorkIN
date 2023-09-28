2955fb356e69127bd272a4581f90e3738dbc1be6
KSSidll <kssidll@users.noreply.github.com>
2023-09-28 22:28:33 +0200

    Abstract workout card into WorkoutCardItem, Update WorkoutCardItem theming

--- app/src/main/java/com/kssidll/workin/presentation/component/SelectWorkoutSubpage.kt
+++ app/src/main/java/com/kssidll/workin/presentation/component/SelectWorkoutSubpage.kt
@@ -1,18 +1,13 @@
 package com.kssidll.workin.presentation.component
 
 import android.content.res.*
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.shape.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.draw.*
-import androidx.compose.ui.res.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.*
-import com.kssidll.workin.R
 import com.kssidll.workin.data.data.*
 import com.kssidll.workin.presentation.theme.*
 
@@ -31,57 +26,12 @@ fun SelectWorkoutSubpage(
 
         LazyColumn {
             items(workouts) {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp, vertical = 8.dp)
-                            .border(
-                                width = OutlinedTextFieldDefaults.UnfocusedBorderThickness,
-                                color = MaterialTheme.colorScheme.outline,
-                                shape = RoundedCornerShape(24.dp)
-                            )
-                            .clickable {
-                                onSelect(it)
-                            }
-                    ) {
-                        Column {
-                            Spacer(modifier = Modifier.height(12.dp))
-
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.Center
-                            ) {
-                                Text(
-                                    text = it.name,
-                                    fontSize = 20.sp
-                                )
-                            }
-
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 24.dp),
-                            ) {
-                                val description = it.description
-                                if (description.isBlank()) {
-                                    Text(
-                                        text = stringResource(id = R.string.workout_no_description_text),
-                                        fontSize = 16.sp,
-                                        modifier = Modifier.alpha(0.45F),
-                                    )
-                                } else {
-                                    Text(
-                                        text = description,
-                                        fontSize = 16.sp,
-                                        modifier = Modifier.alpha(0.8F),
-                                    )
-                                }
-                            }
-
-                            Spacer(modifier = Modifier.height(20.dp))
-                        }
+                WorkoutCardItem(
+                    workout = it,
+                    onSelect = {
+                        onSelect(it)
                     }
-                }
+                )
             }
         }
 
