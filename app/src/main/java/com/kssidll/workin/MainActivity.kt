ebdb4311c1b369d48af60e2c05994d9db4e911f3
KSSidll <kssidll@users.noreply.github.com>
2023-11-04 16:46:49 +0100

    Feat: Add a splashscreen, app icon, and polish language support

--- app/src/main/java/com/kssidll/workin/MainActivity.kt
+++ app/src/main/java/com/kssidll/workin/MainActivity.kt
@@ -8,23 +8,24 @@ import androidx.activity.compose.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.*
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kssidll.workin.ui.theme.*
 import dagger.hilt.android.*
 
 @AndroidEntryPoint
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         @SuppressLint("SourceLockedOrientationActivity")
+        //!! Lock orientation to portrait
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         setContent {
             WorkINTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     Navigation()
                 }
             }
